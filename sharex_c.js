0b1871ba67105735c6b0614c520f8e8fdb520dfc
rxiapvl <44272932+rxiapvl@users.noreply.github.com>
2019-02-23 15:10:19 -0500

    lkhnlm.ffddfgdf

---
+++ sharex_c.js
@@ -0,0 +1,19 @@
+function passWord() {
+    var testV = 1;
+    var pass1 = prompt('Enter The Password',' ');
+    while (testV < 3) {
+    if (!pass1) 
+    history.go(-1);
+    if (pass1.toLowerCase() == "letsmakeadeallll") {
+    alert('Correct, welcome back to the Methods page.');
+    window.open('youcansmdskdmafhij2efqwdfsdfiosdfhi.html');
+    break;
+    } 
+    testV+=1;
+    var pass1 = 
+    prompt('Incorrect password.');
+    }
+    if (pass1.toLowerCase()!="password" & testV ==3) 
+    history.go(-1);
+    return " ";
+    } 
